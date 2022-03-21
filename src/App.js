455f0e673df470448ddb52372f9189cc9191da94
shiponorangetoolz <shipon@orangetoolz.com>
2022-03-21 10:46:02 +0600

    loader

--- src/App.js
+++ src/App.js
@@ -22,40 +22,40 @@ function App() {
 
   return (
     <div className="App">
-      {isLoading && <Link to="/todos"><Button className='py-10'>Loading...</Button></Link>}
-      {!isLoading && 
+      {isLoading ? <Link to="/todos"><Button className='py-10'>Loading...</Button></Link> :
       <div className="col-md-6 mx-auto mt-10">
-        <Link to="/create"><Button className='py-10'>Add</Button></Link>
-        <Link to="/todos"><Button className='py-10'>Todo List</Button></Link>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th> Name</th>
-              <th>Phone</th>
-              <th>Email</th>
-              <th>Address</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-          { contacts !== undefined &&
-          contacts.map((contact,index)=>
-            <tr key={contact.id}>
-              <td>{index + 1}</td>
-              <td>{contact.name}</td>
-              <td>{contact.phone}</td>
-              <td>{contact.email}</td>
-              <td>{contact.address}</td>
-              <td>
-                <Link to={`contact/${contact.id}`}>Edit</Link>
-                <Button onClick={() => handleDeleteClick(contact.id)}>Delete</Button>
-              </td>
-            </tr>
-          )}
-          </tbody>
-        </Table>        
-      </div>}
+      <Link to="/create"><Button className='py-10'>Add</Button></Link>
+      <Link to="/todos"><Button className='py-10'>Todo List</Button></Link>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th> Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        { contacts !== undefined &&
+        contacts.map((contact,index)=>
+          <tr key={contact.id}>
+            <td>{index + 1}</td>
+            <td>{contact.name}</td>
+            <td>{contact.phone}</td>
+            <td>{contact.email}</td>
+            <td>{contact.address}</td>
+            <td>
+              <Link to={`contact/${contact.id}`}>Edit</Link>
+              <Button onClick={() => handleDeleteClick(contact.id)}>Delete</Button>
+            </td>
+          </tr>
+        )}
+        </tbody>
+      </Table>        
+    </div>}
+      
     </div>
   );
 }
